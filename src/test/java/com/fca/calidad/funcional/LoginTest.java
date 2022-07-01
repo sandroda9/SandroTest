0caad31d4be659c16af7f09d975f8d394372258d
patylu001 <patricia.ortegon@gmail.com>
2022-06-30 19:45:03 -0500

    Adding missing imports

--- src/test/java/com/fca/calidad/funcional/LoginTest.java
+++ src/test/java/com/fca/calidad/funcional/LoginTest.java
@@ -12,6 +12,9 @@ import org.apache.commons.io.FileUtils;
 import java.io.File;
 import io.github.bonigarcia.wdm.WebDriverManager;
 
+import org.apache.commons.io.FileUtils;
+import java.io.File;
+import java.io.IOException;
 
 public class LoginTest {
 
